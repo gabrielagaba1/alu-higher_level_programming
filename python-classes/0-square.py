f748131fa9b5a96ced90ca96a933c9eabc37012f
gabrielagaba1 <g.agaba@alustudent.com>
2026-07-22 06:11:39 -0700

    Fix Square class file

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """Module that defines an empty Square class."""
 
 
@@ -9,11 +8,3 @@ class Square:
     This class is currently empty and only serves as a placeholder
     for a square shape, with no attributes or methods defined yet.
     """
-=======
-"""Module that defines a Square class."""
-
-
-class Square:
-    """An empty square."""
-    pass
->>>>>>> 36c5269aee1840dd0b21efec099f8632b1bf0f8d
