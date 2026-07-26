73107a07a7ce093dd54f7543bdaabce2d452cbe7
gabrielagaba1 <g.agaba@alustudent.com>
2026-07-26 10:20:27 -0700

    Add 10-student.py: Student with filterable to_json method

---
+++ python-input_output/10-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Module that defines a Student class with a filterable to_json."""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+
+        Args:
+            first_name: The student's first name.
+            last_name: The student's last name.
+            age: The student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a dictionary representation of this Student.
+
+        Args:
+            attrs: An optional list of attribute names to include.
+                If it's not a list of strings, all attributes are
+                retrieved instead.
+
+        Returns:
+            A dictionary of this Student's attributes, filtered by
+            attrs when applicable.
+        """
+        if isinstance(attrs, list) and all(
+                isinstance(attr, str) for attr in attrs):
+            return {
+                key: value for key, value in self.__dict__.items()
+                if key in attrs
+            }
+        return self.__dict__
